7b82574e534e9db1b90f66114fe4a092025916bc
Ionut-Andrei Baches <ionut.baches@yahoo.com>
2020-09-09 00:37:22 +0300

    fix lint

--- source/content.js
+++ source/content.js
@@ -3,7 +3,7 @@ If the click was on a link, send a message to the background page.
 The message contains the link's URL.
 */
 function notifyExtension(e) {
-	let target = e.target;
+	const { target } = e;
 	while ((target.tagName !== 'A' || !target.href) && target.parentNode) {
 		target = target.parentNode;
 	}
